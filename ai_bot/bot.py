f6621cd781ca2b7483566436b7e0834156314cb8
Namith <namithspider@gmail.com>
2025-11-01 06:37:46 +0530

    deploy

--- ai_bot/bot.py
+++ ai_bot/bot.py
@@ -21,6 +21,7 @@ from typing import Dict, Any
 
 load_dotenv()
 
+
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_ENV = os.getenv("PINECONE_ENV")
